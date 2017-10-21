f7179670def4ac7fde1d213b8a062d0789a25a9c
ahmedabuamra <ah.abuamra@gmail.com>
2017-10-21 13:34:26 +0200

    Update MaxPairwiseProduct.cpp

--- Coursera_AlgorithmicToolbox/week1/MaxPairwiseProduct.cpp
+++ Coursera_AlgorithmicToolbox/week1/MaxPairwiseProduct.cpp
@@ -1,3 +1,24 @@
+/* O(N) solution *//* :D */
+#include<bits/stdc++.h>
+using namespace std;
+int a[200001];
+int main() {
+	int n; cin >> n;
+	for(int i = 0 ; i < n ;i++) cin >> a[i];
+	int ind1 = -1 , ind2 = -1;
+	for(int i = 0 ; i < n ; i++)
+		if(ind1 == -1 || a[i] > a[ind1])
+			ind1 = i;
+
+	for(int i = 0 ; i < n ; i++)
+		if((ind2 == -1 || a[i] > a[ind2]) && (ind1 != ind2))
+			ind2 = i;
+
+	long long ans = (long long)a[ind1] * (long long)a[ind2];
+	cout << ans << endl;
+}
+
+/* O(Nlog(N)) Solution *//*
 #include<bits/stdc++.h>
 using namespace std;
 int a[200001];
@@ -5,6 +26,6 @@ int main() {
 	int n; cin >> n;
 	for(int i = 0 ; i < n ; i++) cin >> a[i];
 	sort(a,a+n);
-	long long ans = a[n-1]*a[n-2];
+	long long ans = (long long)(a[n-1])*(long long)(a[n-2]);
 	cout << ans << endl;
-}
+}*/
